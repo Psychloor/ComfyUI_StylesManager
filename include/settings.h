d283e28ddc973d42b7ee024e91950b46f1696e5b
blomq <blomqvistkevin@hotmail.com>
2025-07-08 18:43:28 +0200

    Add initial headers for main application components and update `.gitignore`

---
+++ include/settings.h
@@ -0,0 +1,32 @@
+﻿//
+// Created by blomq on 2025-07-08.
+//
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QStringList>
+
+class settings {
+public:
+	static settings& instance();
+
+	[[nodiscard]] QStringList recent_files() const;
+	void add_recent_file(const QString& path);
+
+	[[nodiscard]] QString last_directory() const;
+	void set_last_directory(const QString& path);
+
+private:
+	settings() = default;
+
+	void load();
+	void save() const;
+
+	QStringList _recent_files;
+	QString _last_directory;
+};
+
+
+
+#endif //SETTINGS_H
